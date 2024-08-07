511939606edf1355c40d6f901fea789a24f5b556
MrCo0701 <nguyenduyhao07012004@gmail.com>
2024-08-07 15:01:51 +0700

    check day

--- Basic_1/first_app/lib/main.dart
+++ Basic_1/first_app/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: Center(child: Text('Nguyen Duy Hao')),
-    ),
+    home: Scaffold(body: GradientContainer()),
   ));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return const Column(
+      children: [Text('Nguyen duy hao')],
+    );
+  }
 }
