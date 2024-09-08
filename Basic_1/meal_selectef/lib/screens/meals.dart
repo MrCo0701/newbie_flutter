da1670f837959c2c10b49f2757cf0306591fe36b
MrCo0701 <nguyenduyhao07012004@gmail.com>
2024-09-08 22:55:00 +0700

    check day

--- Basic_1/meal_selectef/lib/screens/meals.dart
+++ Basic_1/meal_selectef/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_selectef/models/meal.dart';
+import 'package:meal_selectef/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -36,7 +37,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
           itemCount: meals.length,
-          itemBuilder: (cxt, index) => Text(meals[index].title));
+          itemBuilder: (cxt, index) => MealItem(meal: meals[index]));
     }
 
     return Scaffold(
